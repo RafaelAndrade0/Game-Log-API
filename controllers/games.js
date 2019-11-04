0f6a81a9ff7c7ee1b1ec34dafdc7a8c14aa4e594
Rafael Andrade Silva <rafael.andrade.dev0@gmail.com>
2019-11-04 13:36:12 -0300

    AsyncHandler Middleware

--- controllers/games.js
+++ controllers/games.js
@@ -1,105 +1,58 @@
 const Game = require('../models/Game');
+const asyncHandler = require('../middleware/asyncHandler');
 
 // @desc  Get All Games
 // @route GET api/v1/games
 // @access Public
-exports.getGames = async (req, res, next) => {
-  try {
-    const games = await Game.find();
-    res.status(200).json({
-      success: true,
-      count: games.length,
-      data: games
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      success: false
-    });
-  }
-};
+exports.getGames = asyncHandler(async (req, res, next) => {
+  const games = await Game.find();
+  res.status(200).json({ success: true, count: games.length, data: games });
+});
 
 // @desc  Get a single Game
 // @route GET api/v1/games/:id
 // @access Public
-exports.getGame = async (req, res, next) => {
-  try {
-    const game = await Game.findById(req.params.id);
-    res.status(200).json({
-      success: true,
-      data: game
-    });
-
-    if (!game) {
-      res.status(404).json({ success: false });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      success: false
-    });
+exports.getGame = asyncHandler(async (req, res, next) => {
+  const game = await Game.findById(req.params.id);
+  if (!game) {
+    res.status(404).json({ success: false });
   }
-};
+  res.status(200).json({
+    success: true,
+    data: game
+  });
+});
 
 // @desc  Add a Game
 // @route POST api/v1/games
 // @access Private
-exports.addGame = async (req, res, next) => {
-  try {
-    const game = await Game.create(req.body);
-    res.status(201).json({
-      success: true,
-      data: game
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      success: false
-    });
-  }
-};
+exports.addGame = asyncHandler(async (req, res, next) => {
+  const game = await Game.create(req.body);
+  res.status(201).json({ success: true, data: game });
+});
 
 // @desc  Update a Game
 // @route PUT api/v1/games/:id
 // @access Private
-exports.updateGame = async (req, res, next) => {
-  try {
-    const game = await Game.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+exports.updateGame = asyncHandler(async (req, res, next) => {
+  const game = await Game.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!game) {
-      res.status(404).json({ success: false });
-    }
-
-    res.status(200).json({ success: true, data: game });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      success: false
-    });
+  if (!game) {
+    res.status(404).json({ success: false });
   }
-};
+  res.status(200).json({ success: true, data: game });
+});
 
 // @desc  Delete a Game
 // @route DELETE api/v1/games/:id
 // @access Private
-exports.deleteGame = async (req, res, next) => {
-  try {
-    const game = await Game.findByIdAndDelete(req.params.id);
-    res.status(200).json({
-      success: true,
-      data: game
-    });
-
-    if (!game) {
-      res.status(404).json({ success: false });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      success: false
-    });
+exports.deleteGame = asyncHandler(async (req, res, next) => {
+  const game = await Game.findByIdAndDelete(req.params.id);
+  if (!game) {
+    res.status(404).json({ success: false });
   }
-};
+  res.status(200).json({ success: true, data: {} });
+});
