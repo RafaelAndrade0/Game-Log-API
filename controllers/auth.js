2347eab1ddc0b38acc5f606d4f01b1381f9f9747
Rafael Andrade Silva <rafael.andrade.dev0@gmail.com>
2019-11-07 10:27:04 -0300

    Sign & Get JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,9 @@ const ErrorResponse = require('../utils/errorResponse');
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, role, password } = req.body;
 
-  await User.create({ name, email, role, password });
+  const user = await User.create({ name, email, role, password });
 
-  res.status(201).json({ success: true });
+  const token = user.getSignedJwtToken();
+
+  res.status(201).json({ success: true, token });
 });
