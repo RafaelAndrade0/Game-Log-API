639a32db9159b333b82243860bf3498660502c9b
Rafael <rafael.andrade.dev0@gmail.com>
2019-11-09 19:04:54 -0300

    Forgot Password - Send Email

--- controllers/auth.js
+++ controllers/auth.js
@@ -74,9 +74,32 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   // Get Reset Token
   const resetToken = user.getResetPasswordToken();
 
-  console.log(resetToken);
+  await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({ success: true, data: user });
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of password.
+    Please make a put request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password Reset Token',
+      message
+    });
+
+    res.status(200).json({ success: true, data: 'Email Sent' });
+  } catch (error) {
+    console.log(error);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
 });
 
 const sendTokenResponse = (user, statusCode, res) => {
